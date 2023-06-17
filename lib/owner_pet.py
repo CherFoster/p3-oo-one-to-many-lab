a7532ee14a46cff0c3359f43af157b39611ed19a
Cherise Foster <cherise.foster@outlook.com>
2023-06-17 12:55:27 -0600

    passed test

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type.")
+      
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("The pet must be of type 'Pet'")
+        pet.owner = self
+    
+    def pets(self):
+        return [pet for pet in Pet.all]
+    
+    def sort_pets_by_name(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    
+    def get_sorted_pets(self):
+        return self.sort_pets_by_name()
